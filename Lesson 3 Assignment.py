e603fbf0daffbe130832b5fd162c5437e210ffae
mckennarenee6-cloud <mckennarenee6@gmail.com>
2026-09-23 22:10:09 -0500

    Create Lesson 3 Assignment.py

---
+++ Lesson 3 Assignment.py
@@ -0,0 +1,35 @@
+Instructions
+Using the details that follow, complete the given objectives and include all necessary deliverables requested in the steps.
+
+Create a new Python, .py, file called interest_calculation.py and complete the following objectives. An example can be seen below!
+Create a variable to store an investment amount integer value input from the user
+Use a while loop to confirm the user entered an investment greater than 0 but less than 50,000
+If the investment amount is invalid, provide feedback to the user and ask them to input again
+Create a variable to store an interest rate integer value input from the user
+Use a while loop to confirm the user entered a rate value greater than 0 but less than 15
+If the rate is invalid, provide feedback to the user and ask them to input again
+Create a variable to store an investment duration in years integer value input from the user
+Use a while loop to confirm the user enters a value greater than 0
+if the duration is invalid, provide feedback and receive a new input from the user
+Next, use a loop to calculate the investment compounded monthly
+You will need to convert years to months, this will be a separate variable
+You will need to convert the interest rate to a monthly decimal value
+Divide the interest rate by 12 and then by 100, this will be a separate variable
+You may need another variable to calculate the future total after our calculations
+To perform the calculation, loop through each month with a for loop
+Add the investment amount into the current value of total variable
+Then, calculate the interest by multiplying the current total value by the monthly interest rate
+Be sure to round the calculation for currency
+Add the interest to the current value of the total variable
+Use an if-statement to check if the current month loop is for a full year
+If the current month loop is a full year, output the current year and the current value of the investment
+After finishing the calculation, output the amount of years calculated for, the yearly interest rate input by the user, the monthly investment amount, and finally the total amount of the investment after compounding
+Be sure to round the output for currency
+May require casting to value to output with strings
+Include a print statement at the end of the exercise stating the following, "Completed by, {your name here}" to show who completed the assignment
+Screenshot of output and source code from the exercise showing successful completion of commands (may require multiple screenshots)
+Lesson 3 Example Output
+
+Copy and paste your source code, required screenshots, and other required materials into a word document named in the following format, "First Initial + Last Name - Lesson # Assignment.docx"
+
+Example: JSullwold - Lesson 3 Assignment.docx
