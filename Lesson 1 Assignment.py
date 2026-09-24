59da875a5cb05c8bbdea1eee330f9dadf103faf7
mckennarenee6-cloud <mckennarenee6@gmail.com>
2026-09-23 22:07:48 -0500

    Create Lesson 1 Assignment.py

---
+++ Lesson 1 Assignment.py
@@ -0,0 +1,20 @@
+Instructions
+Complete the following objectives and include all deliverables requested in the steps:
+
+Open the Python shell by entering "python" or "python3" at the command line of your computer. Alternatively, you may search for Python and run the application and a python shell should open accordingly. An example can be seen below.
+First, attempt to write a string, strings are text literals or characters and are contained within quotation marks.
+Then, attempt to create a variable with a string, variables are initialized or assigned using an assignment statement
+To create a variable, we create a variable name we want to call in our code to obtain a value. Next, we assign the name to equal a value using the = symbol. Lastly, we assign it a value. In the case of a string, we provide the string, or in the case of integers or other variable types we assign the corresponding data.
+Next, attempt to create a variable using a string without the quotation marks. Notice the error you receive. We will learn more about variable types in chapter 2, but for now just understanding we need quotation marks is great!
+Lastly, in the python shell we can work with numbers directly to perform calculations and comparisons.
+Try setting a number equal to another number. You will receive an error stating you are unable to assign a literal as a variable. We will learn more about variable assignments in later chapters as well!
+Next, try performing some simple addition, subtraction, multiplication, or division. You should receive the results like a basic calculator would. We can also perform more complex math, but additional functionality may be required for those operations.
+Now, we will perform comparisons using logical operators. We will learn more about this operators in Chapter 2, but for now understand we can perform comparisons between two or more values with these symbols. If you want to test if a value is equal to another we use double equal signs, or "==" to test. Test if two values are equal and the results you receive if they are equal and not equal.
+Include a print statement at the end of the exercise stating the following, "Completed by, {your name here}" to show who completed the assignment
+Screenshot of output and source code from the exercise showing successful completion of commands (may require multiple screenshots)
+Example Image
+
+Copy and paste your source code, required screenshots, and other required materials into a word document named in the following format, "First Initial + Last Name - Lesson # Assignment.docx"
+
+Example: JSullwold - Lesson 1 Assignment.docx
+
