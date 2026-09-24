beb5a75f90f7eca42b4d1952c90b5ae667f5b55b
mckennarenee6-cloud <mckennarenee6@gmail.com>
2026-09-23 22:08:39 -0500

    Create Lesson 2 Assignment.py

---
+++ Lesson 2 Assignment.py
@@ -0,0 +1,22 @@
+Using the details that follow, complete the given objectives and include all necessary deliverables requested in the steps.
+
+Create a new Python, .py, file called age_calculator.py and complete the following objectives. An example can be seen below!
+Create a variable called first_name, which asks the user for their first name
+Create a variable called last_name, which asks the user for their last name
+Create a variable called current_year, which will ask the user for the current year and cast it into a integer value
+Create a variable called birth_year, which will ask the user for their birth year and cast it into an integer value
+Next, create a variable called age, which will calculate the users approximate age using the input current year and birth year
+Then using a single print statement and string concatenation (not an f-string), output a greeting to the first and last names previously input, then on a new line tell the use how old they are this year using the age
+To add a new line within a single print statement you will need to use an escape sequence character 
+Remember, you need to cast any integers or float values into a string when performing concatenation
+Then using a compound assignment operator, add one to the users age
+Lastly, using an f-string, print how old the user will be in the following year
+You will need to add 1 to the current year inside the f-string
+Include a print statement at the end of the exercise stating the following, "Completed by, {your name here}" to show who completed the assignment
+Screenshot of output and source code from the exercise showing successful completion of commands (may require multiple screenshots)
+Lesson 1 Example
+
+Copy and paste your source code, required screenshots, and other required materials into a word document named in the following format, "First Initial + Last Name - Lesson # Assignment.docx"
+
+Example: JSullwold - Lesson 2 Assignment.docx
+
